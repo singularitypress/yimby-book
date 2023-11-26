74e8a12927b3f40c76ef8176fe1603fd65209d1b
singularitypress <jaypandya2k5@gmail.com>
2023-11-26 12:05:37 -0500

    Add Inter font, apply styling to main container

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,18 @@
 import { AppProps } from "next/app";
 import "../app/globals.scss";
+import { Inter } from "next/font/google";
 import { AnimateInContainer } from "@components";
+
+const inter = Inter({ subsets: ["latin"] });
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Component {...pageProps} />
+      <div
+        className={`${inter.className} w-full flex flex-col justify-center items-center`}
+      >
+        <Component {...pageProps} />
+      </div>
       <AnimateInContainer />
     </>
   );
